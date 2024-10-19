fb6a47dc6dd26fe91abe3dbd96757752ef16ec4e
Agape Mugisha <mugishaagape1@gmail.com>
2024-10-19 16:28:27 +0800

    updaded

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -25,7 +25,9 @@ CREATE TABLE Orders(
 );
 CREATE TABLE Orders_Details(
     orderdetailid PRIMARY KEY,
-    order_id FOREIGN KEY,
-    book_id FOREIGN KEY,
+    order_id INT FOREIGN KEY,
+    book_id INT  FOREIGN KEY,
     quantity DOUBLE
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id)
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
