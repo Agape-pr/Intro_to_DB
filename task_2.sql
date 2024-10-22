a9b470f369dbd4ead21032d8a5ff6625671f4b97
Agape-pr <mugishaagapelearning@gmail.com>
2024-10-22 01:39:32 +0000

    updateed

--- task_2.sql
+++ task_2.sql
@@ -1,29 +1,29 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store 
-CREATE TABLE Books(
+CREATE TABLE books(
     book_id PRIMARY KEY,
     title VARCHAR(130),
     price DOUBLE,
     publication_date DATE
 );
-CREATE TABLE Authors(
+CREATE TABLE authors(
     author_id PRIMARY KEY,
     author_name VARCHAR(215)
 );
-CREATE TABLE Customers(
+CREATE TABLE customers(
     customer_id PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215)
     address TEXT
 
 );
-CREATE TABLE Orders(
+CREATE TABLE orders(
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
     
 );
-CREATE TABLE Order_Details(
+CREATE TABLE order_Details(
     orderdetailid PRIMARY KEY,
     order_id INT FOREIGN KEY,
     book_id INT  FOREIGN KEY,
